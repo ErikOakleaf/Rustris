2305be87296e8908eb5be90efe9c79b8f9ba026c
ErikOakleaf <oakl33f123@gmail.com>
2024-11-15 14:12:04 +0100

    set minumum window size and resizable window

--- src/game.rs
+++ src/game.rs
@@ -10,14 +10,21 @@ pub struct Game {
 }
 
 impl Game {
+    const WINDOW_WIDTH: u32 = 1000; 
+    const WINDOW_HEIGHT: u32 = 800;
+
     pub fn new() -> Result<Self, String>{
         let sdl_context = sdl2::init()?;
         let video_subsystem = sdl_context.video()?;
-        let window = video_subsystem
-            .window("rusty-tetris", 800, 600)
+        let mut window = video_subsystem
+            .window("rusty-tetris", Self::WINDOW_WIDTH, Self::WINDOW_HEIGHT)
             .position_centered()
+            .resizable()
             .build()
             .map_err(|e| e.to_string())?;
+
+        window.set_minimum_size(Self::WINDOW_WIDTH, Self::WINDOW_HEIGHT).map_err(|e| e.to_string())?;
+
         let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
         let event_pump = sdl_context.event_pump()?;
 
